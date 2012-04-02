f9922d3240551422851c8476d7c84dd88225b919
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-04-02 08:59:12 +0300

    Added wrapper registry call, removed wrong setter

--- src/main/scala/vaadin/scala/example/wrapper/WrappedIntStepper.scala
+++ src/main/scala/vaadin/scala/example/wrapper/WrappedIntStepper.scala
@@ -5,12 +5,11 @@ import org.vaadin.risto.stepper.IntStepper
 
 class WrappedIntStepper(implicit wrapper: WrapperRegistry) extends AbstractField {
   override val p = new IntStepper
+  wr.put(this)
 
   def maxValue_=(max: Int) = p.setMaxValue(max)
   def maxValue: Int = p.getMaxValue
 
   def minValue_=(max: Int) = p.setMaxValue(max)
   def minValue: Int = p.getMaxValue
-
-  def stepAmount_=(amount: Int) = p.setStepAmount(amount)
 }
