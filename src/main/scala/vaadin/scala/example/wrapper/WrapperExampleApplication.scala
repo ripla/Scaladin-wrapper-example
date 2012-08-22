5d24897d795615fab3e605b9e472f46c9a655404
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-08-22 13:10:12 -0400

    Update to release version of Scaladin 2.0

--- src/main/scala/vaadin/scala/example/wrapper/WrapperExampleApplication.scala
+++ src/main/scala/vaadin/scala/example/wrapper/WrapperExampleApplication.scala
@@ -6,7 +6,11 @@ import org.vaadin.risto.stepper.IntStepper
 class WrapperExampleApplication extends Application(title = "Scaladin wrapper example") {
   override def main = new VerticalLayout {
     components += Label("This Vaadin app uses ScalaWrappers!")
-    components += new IntStepper with ScaladinWrapper
+    
+    //method 1: one line integration with ScaladinWrapper
+    components += new IntStepper with ScaladinWrapper 
+    
+    //method 2: create a separate wrapper class
     components += new WrappedIntStepper
   }
 
