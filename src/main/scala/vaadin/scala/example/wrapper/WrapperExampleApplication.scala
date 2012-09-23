ac3e38758cadd510a2e6f2ef979a4ad11182ea00
Risto Yrjänä <risto.yrjana@vaadin.com>
2012-09-23 14:05:48 +0300

    Use 2.1 style wrapping

--- src/main/scala/vaadin/scala/example/wrapper/WrapperExampleApplication.scala
+++ src/main/scala/vaadin/scala/example/wrapper/WrapperExampleApplication.scala
@@ -2,16 +2,17 @@ package vaadin.scala.example.wrapper
 
 import vaadin.scala._
 import org.vaadin.risto.stepper.IntStepper
+import vaadin.scala.mixins.ComponentMixin
 
 class WrapperExampleApplication extends Application(title = "Scaladin wrapper example") {
   override def main = new VerticalLayout {
-    components += Label("This Vaadin app uses ScalaWrappers!")
-    
     //method 1: one line integration with ScaladinWrapper
-    components += new IntStepper with ScaladinWrapper 
-    
+    //access wrapped class via the typed p -variable
+    val wrappedStepper = ScaladinWrapper.wrapComponent(new IntStepper with ComponentMixin)
+    wrappedStepper.p.setManualInputAllowed(false)
+    components += wrappedStepper
+
     //method 2: create a separate wrapper class
     components += new WrappedIntStepper
   }
-
 }
